90e29038f9ee7f4b23ba47a9b291d7e9e6c3f7f0
Tharindu Hasthika <tharindubathigama@gmail.com>
2017-12-28 04:50:01 +0530

    finished assignment 12

--- dataalgo/as12/04.c
+++ dataalgo/as12/04.c
@@ -2,23 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 10
+
 typedef struct {
-    char id[6];
+    char id[7];
     float math;
     float chem;
 } StudentInfo;
 
 void displayStudent(StudentInfo *s);
 void insertStudent(StudentInfo *s);
-int searchStudent(StudentInfo *s, char id[6]);
+int searchStudent(StudentInfo *arr, char *id);
 float studentAvg(StudentInfo *s);
+void displayStudentsAbove(StudentInfo *arr, float avg);
 
 int main() {
 
-    StudentInfo arr[10];
+    StudentInfo arr[MAX];
 
-    insertStudent(&arr[0]);
-    displayStudent(&arr[0]);
+    for(int i = 0; i < MAX; i++) {
+	printf("Insert student %d\n", i+1);
+	insertStudent(&arr[i]);
+    }
+
+    char id[7];
+    printf("Enter the ID to search: ");
+    scanf("%s", id);
+    id[6] = '\0';
+
+    int r = searchStudent(arr, id);
+    if(r != -1) {
+	printf("Student Found!\n");
+	displayStudent(&arr[r]);
+    } else {
+	printf("Student Not Found!\n");
+    }
+
+    float avg;
+    printf("Enter the lower bound of the search: ");
+    scanf("%f", &avg);
+
+    displayStudentsAbove(arr, avg);
 
     
     return 0;
@@ -34,9 +58,34 @@ void displayStudent(StudentInfo *s) {
 
 void insertStudent(StudentInfo *s) {
     printf("Enter Student ID: ");
-    scanf("%6s", s->id);
+    scanf("%s", s->id);
     printf("Enter Math Marks: ");
     scanf("%f", &s->math);
     printf("Enter Chemistry Marks: ");
     scanf("%f", &s->chem);
+    s->id[6] = '\0';
+}
+
+int searchStudent(StudentInfo *arr, char *id) {
+    for(int i = 0; i < MAX; i++) {
+	if(strcmp((&arr[i])->id, id) == 0) {
+	    return i;
+	}
+    }
+    return -1;
+}
+
+float studentAvg(StudentInfo *s) {
+    float avg = (s->math + s->chem) / 2.f;
+    return avg;
+}
+
+void displayStudentsAbove(StudentInfo *arr, float avg) {
+
+    printf("Students above the %.2f average\n", avg);
+    for(int i = 0; i < MAX; i++) {
+	if(avg < studentAvg(&arr[i])) {
+	    displayStudent(&arr[i]);
+	}
+    }
 }
