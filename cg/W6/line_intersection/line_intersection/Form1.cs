d06fad22c3b51603732a32d3b96f66f5e79e852b
Tharindu Hasthika <tharindubathigama@gmail.com>
2018-07-08 12:45:40 +0530

    cg new assignment

---
+++ cg/W6/line_intersection/line_intersection/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace line_intersection
+{
+    public partial class Form1 : Form
+    {
+
+        float x1, y1, x2, y2, x3, y3, x4, y4;
+
+        private void DisplayPoint(float x, float y)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            g.DrawLine(Pens.Black, x, pnlMain.Height - y, x + 0.1F, pnlMain.Height - y);
+        }
+
+        private void DisplayLine(float x1, float y1, float x2, float y2)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            if ((x1 == x2) && (y1 == y2))
+            {
+                DisplayPoint(x1, y1);
+            }
+            else
+            {
+                g.DrawLine(Pens.Black, x1, pnlMain.Height - y1, x2, pnlMain.Height - y2);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                x1 = float.Parse(txtX1.Text);
+                y1 = float.Parse(txtY1.Text);
+                x2 = float.Parse(txtX2.Text);
+                y2 = float.Parse(txtY2.Text);
+                x3 = float.Parse(txtX3.Text);
+                y3 = float.Parse(txtY3.Text);
+                x4 = float.Parse(txtX4.Text);
+                y4 = float.Parse(txtY4.Text);
+
+                DisplayLine(x1, y1, x2, y2);
+                DisplayLine(x3, y3, x4, y4);
+            }
+            catch
+            {
+                MessageBox.Show("Enter Only Numbers!");
+            }
+        }
+
+        private bool onSegment(float x1, float y1, float x2, float y2, float x, float y)
+        {
+            float ux = Math.Max(x1, x2);
+            float lx = Math.Min(x1, x2);
+            float uy = Math.Max(y1, y2);
+            float ly = Math.Max(y1, y2);
+            if (x <= ux && x >= lx && y <= uy && y >= ly)
+                return true;
+            return false;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            float t1, t2, t3, t4;
+
+            t1 = getT(x1, y1, x2, y2, x3, y3);
+            t2 = getT(x1, y1, x2, y2, x4, y4);
+
+            t3 = getT(x3, y3, x4, y4, x1, y1);
+            t4 = getT(x3, y3, x4, y4, x2, y2);
+
+            if (t1 * t2 > 0 && t3 * t4 > 0)
+            {
+                lblStatus.Text = "Intersects!";
+                return;
+            }
+
+            if (t1 == 0 && onSegment(x1, y1, x2, y2, x3, y3))
+            {
+                lblStatus.Text = "Intersects!";
+                return;
+            }
+
+            if (t2 == 0 && onSegment(x1, y1, x2, y2, x4, y4))
+            {
+                lblStatus.Text = "Intersects!";
+                return;
+            }
+
+            if (t3 == 0 && onSegment(x3, y3, x4, y4, x1, y1))
+            {
+                lblStatus.Text = "Intersects!";
+                return;
+            }
+
+            if (t4 == 0 && onSegment(x3, y3, x4, y4, x2, y2))
+            {
+                lblStatus.Text = "Intersects!";
+                return;
+            }
+
+            lblStatus.Text = "Doesn't Intersect";
+        }
+
+        private float getT(float x1, float y1, float x2, float y2, float x3, float y3)
+        {
+            return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+        }
+
+    }
+}
---
+++ cg/W6/q1/q1/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace q1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDrawT = new System.Windows.Forms.Button();
+            this.btnDrawP = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtY1 = new System.Windows.Forms.TextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtY2 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.txtY3 = new System.Windows.Forms.TextBox();
+            this.txtX3 = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Location = new System.Drawing.Point(13, 13);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(300, 300);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtY3);
+            this.groupBox1.Controls.Add(this.txtX3);
+            this.groupBox1.Controls.Add(this.txtY2);
+            this.groupBox1.Controls.Add(this.txtX2);
+            this.groupBox1.Controls.Add(this.txtY1);
+            this.groupBox1.Controls.Add(this.txtX1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(320, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Triangle";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtY);
+            this.groupBox2.Controls.Add(this.txtX);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(320, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 51);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Point";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblStatus);
+            this.groupBox3.Controls.Add(this.btnCheck);
+            this.groupBox3.Location = new System.Drawing.Point(320, 268);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(177, 46);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Detection";
+            // 
+            // btnDrawT
+            // 
+            this.btnDrawT.Location = new System.Drawing.Point(320, 239);
+            this.btnDrawT.Name = "btnDrawT";
+            this.btnDrawT.Size = new System.Drawing.Size(85, 23);
+            this.btnDrawT.TabIndex = 4;
+            this.btnDrawT.Text = "Draw Triangle";
+            this.btnDrawT.UseVisualStyleBackColor = true;
+            this.btnDrawT.Click += new System.EventHandler(this.btnDrawT_Click);
+            // 
+            // btnDrawP
+            // 
+            this.btnDrawP.Location = new System.Drawing.Point(410, 239);
+            this.btnDrawP.Name = "btnDrawP";
+            this.btnDrawP.Size = new System.Drawing.Size(87, 23);
+            this.btnDrawP.TabIndex = 5;
+            this.btnDrawP.Text = "Draw Point";
+            this.btnDrawP.UseVisualStyleBackColor = true;
+            this.btnDrawP.Click += new System.EventHandler(this.btnDrawP_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(6, 17);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(35, 19);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(39, 20);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(115, 19);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(39, 20);
+            this.txtY.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "x1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "y1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "y2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "x2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(97, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "y3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "x3";
+            // 
+            // txtY1
+            // 
+            this.txtY1.Location = new System.Drawing.Point(115, 16);
+            this.txtY1.Name = "txtY1";
+            this.txtY1.Size = new System.Drawing.Size(39, 20);
+            this.txtY1.TabIndex = 5;
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(35, 16);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(39, 20);
+            this.txtX1.TabIndex = 4;
+            // 
+            // txtY2
+            // 
+            this.txtY2.Location = new System.Drawing.Point(115, 42);
+            this.txtY2.Name = "txtY2";
+            this.txtY2.Size = new System.Drawing.Size(39, 20);
+            this.txtY2.TabIndex = 7;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(35, 42);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(39, 20);
+            this.txtX2.TabIndex = 6;
+            // 
+            // txtY3
+            // 
+            this.txtY3.Location = new System.Drawing.Point(115, 68);
+            this.txtY3.Name = "txtY3";
+            this.txtY3.Size = new System.Drawing.Size(39, 20);
+            this.txtY3.TabIndex = 9;
+            // 
+            // txtX3
+            // 
+            this.txtX3.Location = new System.Drawing.Point(35, 68);
+            this.txtX3.Name = "txtX3";
+            this.txtX3.Size = new System.Drawing.Size(39, 20);
+            this.txtX3.TabIndex = 8;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(97, 22);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(33, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 326);
+            this.Controls.Add(this.btnDrawP);
+            this.Controls.Add(this.btnDrawT);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnDrawT;
+        private System.Windows.Forms.Button btnDrawP;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtY3;
+        private System.Windows.Forms.TextBox txtX3;
+        private System.Windows.Forms.TextBox txtY2;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TextBox txtY1;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
