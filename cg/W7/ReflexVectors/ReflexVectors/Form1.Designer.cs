689a6215735475ee358bace4b78571c9e13d1951
Tharindu Hasthika <tharindubathigama@gmail.com>
2018-07-16 20:40:31 +0530

    CG assignment W7

---
+++ cg/W7/ReflexVectors/ReflexVectors/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ReflexVectors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.gbDoneDrawing = new System.Windows.Forms.GroupBox();
+            this.btnDoneDrawing = new System.Windows.Forms.Button();
+            this.gbReflexVertices = new System.Windows.Forms.GroupBox();
+            this.btnCalculateReflexVertices = new System.Windows.Forms.Button();
+            this.gbClear = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.gbDoneDrawing.SuspendLayout();
+            this.gbReflexVertices.SuspendLayout();
+            this.gbClear.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.White;
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Location = new System.Drawing.Point(12, 12);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(300, 300);
+            this.pnlMain.TabIndex = 0;
+            this.pnlMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlMain_MouseUp);
+            // 
+            // gbDoneDrawing
+            // 
+            this.gbDoneDrawing.Controls.Add(this.btnDoneDrawing);
+            this.gbDoneDrawing.Location = new System.Drawing.Point(318, 12);
+            this.gbDoneDrawing.Name = "gbDoneDrawing";
+            this.gbDoneDrawing.Size = new System.Drawing.Size(200, 55);
+            this.gbDoneDrawing.TabIndex = 2;
+            this.gbDoneDrawing.TabStop = false;
+            this.gbDoneDrawing.Text = "Done Drawing";
+            // 
+            // btnDoneDrawing
+            // 
+            this.btnDoneDrawing.Location = new System.Drawing.Point(10, 19);
+            this.btnDoneDrawing.Name = "btnDoneDrawing";
+            this.btnDoneDrawing.Size = new System.Drawing.Size(177, 23);
+            this.btnDoneDrawing.TabIndex = 1;
+            this.btnDoneDrawing.Text = "Done";
+            this.btnDoneDrawing.UseVisualStyleBackColor = true;
+            this.btnDoneDrawing.Click += new System.EventHandler(this.btnSetVertexCount_Click);
+            // 
+            // gbReflexVertices
+            // 
+            this.gbReflexVertices.Controls.Add(this.btnCalculateReflexVertices);
+            this.gbReflexVertices.Enabled = false;
+            this.gbReflexVertices.Location = new System.Drawing.Point(318, 73);
+            this.gbReflexVertices.Name = "gbReflexVertices";
+            this.gbReflexVertices.Size = new System.Drawing.Size(200, 58);
+            this.gbReflexVertices.TabIndex = 4;
+            this.gbReflexVertices.TabStop = false;
+            this.gbReflexVertices.Text = "Reflex Vertices";
+            // 
+            // btnCalculateReflexVertices
+            // 
+            this.btnCalculateReflexVertices.Location = new System.Drawing.Point(10, 20);
+            this.btnCalculateReflexVertices.Name = "btnCalculateReflexVertices";
+            this.btnCalculateReflexVertices.Size = new System.Drawing.Size(177, 23);
+            this.btnCalculateReflexVertices.TabIndex = 0;
+            this.btnCalculateReflexVertices.Text = "Calculate";
+            this.btnCalculateReflexVertices.UseVisualStyleBackColor = true;
+            this.btnCalculateReflexVertices.Click += new System.EventHandler(this.btnCalculateReflexVertices_Click);
+            // 
+            // gbClear
+            // 
+            this.gbClear.Controls.Add(this.btnClear);
+            this.gbClear.Location = new System.Drawing.Point(318, 137);
+            this.gbClear.Name = "gbClear";
+            this.gbClear.Size = new System.Drawing.Size(200, 62);
+            this.gbClear.TabIndex = 5;
+            this.gbClear.TabStop = false;
+            this.gbClear.Text = "Clear";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(10, 20);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(177, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 325);
+            this.Controls.Add(this.gbClear);
+            this.Controls.Add(this.gbReflexVertices);
+            this.Controls.Add(this.gbDoneDrawing);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbDoneDrawing.ResumeLayout(false);
+            this.gbReflexVertices.ResumeLayout(false);
+            this.gbClear.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.GroupBox gbDoneDrawing;
+        private System.Windows.Forms.Button btnDoneDrawing;
+        private System.Windows.Forms.GroupBox gbReflexVertices;
+        private System.Windows.Forms.Button btnCalculateReflexVertices;
+        private System.Windows.Forms.GroupBox gbClear;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ cg/W7/ReflexVectors/ReflexVectors/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReflexVectors
+{
+
+    public partial class Form1 : Form
+    {
+
+        int mVertexCount = 0;
+        List<Vertex> mVertices = new List<Vertex>();
+        bool mDrawing = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //mVertexCount = 3;
+            //mCurrentVertices = 3;
+            //Vertex v = new Vertex();
+            //v.x = 10;
+            //v.y = 10;
+            //mVertices.Add(v);
+            //v = new Vertex();
+            //v.x = 110;
+            //v.y = 10;
+            //mVertices.Add(v);
+            //v = new Vertex();
+            //v.x = 110;
+            //v.y = 110;
+            //mVertices.Add(v);
+            //v = new Vertex();
+            //v.x = 210;
+            //v.y = 250;
+            //mVertices.Add(v);
+            //gbVertexCount.Enabled = false;
+            //gbReflexVertices.Enabled = true;
+        }
+
+        private void btnSetVertexCount_Click(object sender, EventArgs e)
+        {
+            if (mVertexCount < 3)
+            {
+                MessageBox.Show("Must have atleast 3 vertices!");
+                return;
+            }
+            drawLine(mVertices[mVertexCount - 1], mVertices[0]);
+            mDrawing = false;
+            gbDoneDrawing.Enabled = false;
+            gbReflexVertices.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pnlMain.CreateGraphics().Clear(Color.White);
+            mDrawing = true;
+            mVertexCount = 0;
+            gbDoneDrawing.Enabled = true;
+            gbReflexVertices.Enabled = false;
+            mVertices.Clear();
+        }
+
+        private void btnCalculateReflexVertices_Click(object sender, EventArgs e)
+        {
+            drawPolygon();
+
+            List<Vertex> reflexVertices = new List<Vertex>();
+
+            Vertex v1, v2, a, b;
+            int prevS = 1;
+            int s = 0;
+
+            for (int i = 0; i < mVertices.Count; i++)
+            {
+                v1 = mVertices[i % mVertices.Count];
+                v2 = mVertices[(i + 1) % mVertices.Count];
+                a = mVertices[(i + 2) % mVertices.Count];
+                s = getT(v1, v2, a);
+                for (int j = 0; j < mVertices.Count - 3; j++)
+                {
+                    a = mVertices[(j + i + 3) % mVertices.Count];
+                    prevS = s;
+                    s = getT(v1, v2, a);
+                    Console.WriteLine("prevS: " + prevS + " s: " + s);
+                    if (prevS != s)
+                    {
+                        reflexVertices.Add(v2);
+                        break;
+                    }
+                }
+                //b = mVertices[(i + 2) % mVertices.Count];
+                //int t = i - 1;
+                //if (t < 0)
+                //{
+                //    t = mVertices.Count + t;
+                //}
+                //a = mVertices[Math.Abs(t) % mVertices.Count];
+                //s = getT(v1, v2, a) * getT(v1, v2, b);
+                //if (prevS == -1 && s == -1)
+                //{
+                //    reflexVertices.Add(v1);
+                //}
+                //else
+                //{
+                //    prevS = s;
+                //}
+
+            }
+
+            for (int i = 0; i < reflexVertices.Count; i++)
+            {
+                v1 = reflexVertices[i];
+                drawCircle(v1, 10.0f, Pens.Red);
+            }
+
+        }
+
+        private void pnlMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            Vertex v = new Vertex
+            {
+                x = e.X,
+                y = pnlMain.Height - e.Y
+            };
+            mVertices.Add(v);
+            mVertexCount++;
+            if (mVertexCount > 1)
+            {
+                drawLine(mVertices[mVertexCount - 1], mVertices[mVertexCount - 2]);
+            }
+        }
+
+        private void drawPolygon()
+        {
+            Vertex v1, v2;
+            for (int i = 0; i < mVertices.Count; i++)
+            {
+                v1 = mVertices[i % mVertices.Count];
+                v2 = mVertices[(i + 1) % mVertices.Count];
+                drawLine(v1, v2);
+            }
+        }
+        private void drawPoint(Vertex v)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            g.DrawLine(Pens.Black, v.x, pnlMain.Height - v.y, v.x + 0.1F, pnlMain.Height - v.y);
+        }
+
+        private void drawPoint(Vertex v, Pen pen)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            g.DrawLine(pen, v.x, pnlMain.Height - v.y, v.x + 0.1F, pnlMain.Height - v.y);
+        }
+
+        private void drawCircle(Vertex v, float size, Pen pen)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            g.DrawEllipse(pen, v.x - (size / 2), (pnlMain.Height - v.y - (size / 2)), size, size);
+        }
+
+        private void drawLine(Vertex v1, Vertex v2, Pen pen)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            if ((v1.x == v2.x) && (v1.y == v2.y))
+            {
+                drawPoint(v1, pen);
+            }
+            else
+            {
+                g.DrawLine(pen, v1.x, pnlMain.Height - v1.y, v2.x, pnlMain.Height - v2.y);
+            }
+        }
+
+        private void drawLine(Vertex v1, Vertex v2)
+        {
+            Graphics g = pnlMain.CreateGraphics();
+            if ((v1.x == v2.x) && (v1.y == v2.y))
+            {
+                drawPoint(v1);
+            }
+            else
+            {
+                g.DrawLine(Pens.Black, v1.x, pnlMain.Height - v1.y, v2.x, pnlMain.Height - v2.y);
+            }
+        }
+
+        private int getT(Vertex v1, Vertex v2, Vertex v3)
+        {
+            float s = v1.x * (v2.y - v3.y) + v2.x * (v3.y - v1.y) + v3.x * (v1.y - v2.y);
+            if (s > 0) return 1;
+            if (s < 0) return -1;
+            return 0;
+        }
+    }
+
+    class Vertex
+    {
+        public float x;
+        public float y;
+    }
+}
