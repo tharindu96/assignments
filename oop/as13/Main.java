9cce01479fe30c0d8f3dc801ca443f34aeef42fe
Tharindu Hasthika <tharindubathigama@gmail.com>
2018-09-19 14:24:26 +0530

    as13 - testing

---
+++ oop/as13/Main.java
@@ -0,0 +1,15 @@
+package as13;
+
+import as13.Side;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("HOK!");
+
+        Side s = new Side();
+
+        s.test();
+    }
+
+}
---
+++ oop/as13/Side.java
@@ -0,0 +1,8 @@
+package as13;
+
+
+public class Side {
+    public void test() {
+        System.out.println("Hello From SIde!");
+    }
+}
