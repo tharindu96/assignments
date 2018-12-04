9ce5f4763e2abe4d4a8f615f46807c8f219b35c7
Tharindu Hasthika <tharindubathigama@gmail.com>
2018-12-04 09:19:00 +0530

    new script phpfix.sh

---
+++ scripts/phpfix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]
+then
+    echo "must provide the php.ini file"
+    exit 1
+fi
+
+PHPINI=$1
+
+sudo cp $PHPINI $PHPINI.backup
+
+sudo sed -i -e 's/error_reporting = .*/error_reporting = E_ALL/' $PHPINI
+sudo sed -i -e 's/display_errors = .*/display_errors = On/' $PHPINI
+
+sudo systemctl restart apache2
